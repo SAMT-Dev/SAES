a369e5b0adaafe67511bf48a5a1efd093efb9041
HVCsano <hvcsano@proton.me>
2025-03-14 00:01:50 +0100

    removed stores dir init

--- api/src/init.rs
+++ api/src/init.rs
@@ -1,27 +1,12 @@
 use core::panic;
 use std::{
-    fs::{self, File},
+    fs::{self},
     path::Path,
 };
 
 use saes_shared::sql::test_db_conn;
 
 use crate::config::loader::get_config;
-
-async fn stores_dir_init() {
-    let stores_dir = Path::new("stores");
-    if !stores_dir.exists() {
-        fs::create_dir(stores_dir).expect("Store dir létrehozása sikertelen");
-    }
-    let maintenance_path = Path::new("stores/maintenance.store");
-    if maintenance_path.exists() == false {
-        File::create(maintenance_path).expect("maintenance.store létrehozása sikertelen");
-    }
-    let announcement_path = Path::new("stores/announcement.store");
-    if announcement_path.exists() == false {
-        File::create(announcement_path).expect("announcement.store létrehozása sikertelen");
-    }
-}
 
 fn image_init() {
     let image_dir = Path::new("public");
@@ -46,7 +31,6 @@ fn image_tmp_init() {
 pub async fn main() {
     get_config().await;
     test_db_conn().await;
-    stores_dir_init().await;
     image_init();
     image_tmp_init();
 }
