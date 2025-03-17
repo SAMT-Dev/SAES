d9ed4a08d924ac7d3c29cef9758bb516c56c5338
HVCsano <hvcsano@proton.me>
2025-03-17 19:30:14 +0100

    added faction shortname

--- api/src/utils/middle.rs
+++ api/src/utils/middle.rs
@@ -24,6 +24,7 @@ pub struct DiscordUser {
 pub struct FactionRecord {
     pub factionid: i8,
     pub factionname: String,
+    pub factionshortname: String,
     pub positionid: i8,
     pub positionname: String,
     pub shiftid: i8,
