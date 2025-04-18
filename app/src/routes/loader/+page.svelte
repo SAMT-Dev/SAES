5272d03df79e9ba274b06b4f61c1728d507d7e83
HVCsano <hvcsano@proton.me>
2025-04-19 01:00:39 +0200

    please work

--- app/src/routes/loader/+page.svelte
+++ app/src/routes/loader/+page.svelte
@@ -5,6 +5,7 @@
 	import { relaunch } from '@tauri-apps/plugin-process';
 	import { onMount } from 'svelte';
 	import { listen } from '@tauri-apps/api/event';
+
 	let text = $state('Frissítések keresése');
 	let ver = $state('');
 	let envserr = $state(false);
@@ -15,50 +16,46 @@
 
 	onMount(async () => {
 		ver = await getVersion();
-		setTimeout(async () => {
-			const update = await check();
-			if (update) {
-				text = 'Frissítés találva';
-				console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
-				let downloaded = 0;
-				let contentLength = 0;
-				// alternatively we could also call update.download() and update.install() separately
-				await update.downloadAndInstall((event) => {
-					switch (event.event) {
-						case 'Started':
-							contentLength = event.data.contentLength!;
-							console.log(`started downloading ${event.data.contentLength} bytes`);
-							break;
-						case 'Progress':
-							downloaded += event.data.chunkLength;
-							text = 'Letöltés: ' + Math.round((downloaded / contentLength) * 100) + '%';
-							console.log(`downloaded ${downloaded} from ${contentLength}`);
-							break;
-						case 'Finished':
-							console.log('download finished');
-							text = 'Telepítés';
-							break;
-					}
-				});
+		const update = await check();
+		if (update) {
+			text = 'Frissítés előkészítése';
+			console.log(`found update ${update.version} from ${update.date} with notes ${update.body}`);
+			let downloaded = 0;
+			let contentLength = 0;
+			// alternatively we could also call update.download() and update.install() separately
+			await update.downloadAndInstall((event) => {
+				switch (event.event) {
+					case 'Started':
+						contentLength = event.data.contentLength!;
+						console.log(`started downloading ${event.data.contentLength} bytes`);
+						break;
+					case 'Progress':
+						downloaded += event.data.chunkLength;
+						text = 'Letöltés: ' + Math.round((downloaded / contentLength) * 100) + '%';
+						console.log(`downloaded ${downloaded} from ${contentLength}`);
+						break;
+					case 'Finished':
+						console.log('download finished');
+						text = 'Telepítés';
+						break;
+				}
+			});
 
-				console.log('update installed');
-				text = 'Frissítés kész, újraindítás';
-				await relaunch();
-			}
-			text = 'ENV ellenőrzése';
-			let envs: string = await invoke('check_envs');
-			if (envs === 'ok') {
-				text = 'App indítása';
-				setTimeout(() => {
-					invoke('update_done');
-				}, 500);
-			} else if (envs === 'multiple') {
-				envserr = true;
-				text = '';
-			} else {
-				await relaunch();
-			}
-		}, 300);
+			console.log('update installed');
+			text = 'Frissítés kész, újraindítás';
+			await relaunch();
+		}
+		text = 'ENV ellenőrzése';
+		let envs: string = await invoke('check_envs');
+		if (envs === 'ok') {
+			text = 'App indítása';
+			setTimeout(() => {
+				invoke('update_done');
+			}, 500);
+		} else if (envs === 'multiple') {
+			envserr = true;
+			text = '';
+		}
 	});
 </script>
 
@@ -67,7 +64,7 @@
 >
 	<div class="flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 		<img src="/favicon.png" class="w-[200px] m-auto" alt="" />
-		<h1 class="font-bold text-3xl text-white w-screen">SAMT App</h1>
+		<h1 class="font-bold text-3xl text-white w-screen">SAMT</h1>
 		<h2 class="text-gray-300 font-light">{text}</h2>
 		{#if envserr}
 			<h2 class="text-red-500 font-light">.env beolvasása sikertelen.</h2>
