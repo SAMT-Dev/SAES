c1328613f9f7a0b20080f9364b68f0ec3473693a
HVCsano <hvcsano@proton.me>
2025-04-18 23:04:41 +0200

    better

--- web/src/routes/logout/+page.server.ts
+++ web/src/routes/logout/+page.server.ts
@@ -1,10 +1,12 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ cookies }) => {
-	cookies.delete('auth_token', {
-		path: '/',
-		domain: process.env.NODE_ENV === 'development' ? 'localhost' : 'samt.hu'
+export const load = (async ({ cookies, url }) => {
+	cookies.delete("auth_token", {
+		path: "/",
+		domain: process.env.NODE_ENV === "development"
+			? "localhost"
+			: url.hostname,
 	});
-	throw redirect(302, '/');
+	throw redirect(302, "/");
 }) satisfies PageServerLoad;
