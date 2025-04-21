4871554e914c3c93e152679da02261dfd6eb9411
HVCsano <hvcsano@proton.me>
2025-04-22 01:14:03 +0200

    added gmod screen

---
+++ web/src/routes/gmod/+page.svelte
@@ -0,0 +1,4 @@
+<div class="translate absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+	<img src="/sckk_icon.png" alt="" class="h-[100vh]" />
+	<audio src="/taxi_full.mp3" autoplay></audio>
+</div>
