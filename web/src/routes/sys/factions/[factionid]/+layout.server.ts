7e9372e38cf4f6f87ed9f11db6102c2942fe214d
HVCsano <hvcsano@proton.me>
2025-05-20 12:01:28 +0200

    polished faction info page

---
+++ web/src/routes/sys/factions/[factionid]/+layout.server.ts
@@ -0,0 +1,28 @@
+import { apiUrl } from "$lib/api";
+import type { LayoutServerLoad } from "./$types";
+
+export const load = (async ({ cookies, params }) => {
+    let auth = cookies.get("auth_token")!;
+    try {
+        let get = await fetch(`${apiUrl}/sys/getfactions`, {
+            headers: { cookie: auth },
+        });
+        if (get.ok) {
+            let data: Record<string, {
+                name: string;
+                shortname: string;
+                archived: boolean;
+                managed: boolean;
+                icon: string | undefined;
+            }> = await get.json();
+            return {
+                factinfo: data[params.factionid],
+                id: params.factionid,
+            };
+        }
+    } catch (e) {
+        return {
+            error: e,
+        };
+    }
+}) satisfies LayoutServerLoad;
---
+++ web/src/routes/sys/factions/[factionid]/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+	let url = `/sys/factions/${data.id}`;
+</script>
+
+<div
+	class="mb-2 flex w-screen items-center justify-between bg-indigo-600 px-8 text-center text-white"
+>
+	<h1 class="font-bold">{data.factinfo?.name}</h1>
+	<div class="flex items-center justify-center gap-2">
+		<a href={url}>Információk</a>
+		<a href={`${url}/positions`}>Pozíciók</a>
+		<a href={`${url}/shifts`}>Műszakok</a>
+	</div>
+</div>
+
+{@render children()}
