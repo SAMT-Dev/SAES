5f30ffac4da12fdcc46fa484eb64d5466f0e1219
HVCsano <hvcsano@proton.me>
2025-06-01 18:32:45 +0200

    fixed sys endpoints

--- web/src/routes/web-api/sys/faction/[faction]/+server.ts
+++ web/src/routes/web-api/sys/faction/[faction]/+server.ts
@@ -1,17 +1,20 @@
-import { apiUrl } from '$lib/api';
-import type { RequestHandler } from '@sveltejs/kit';
+import { apiUrl } from "$lib/api";
+import type { RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ request, cookies, params }) => {
-	const dcauth = cookies.get('auth_token') as string;
+	const dcauth = cookies.get("auth_token") as string;
 	const body = await request.json();
-	const post = await fetch(`${apiUrl}/ucp/sys/config/faction-post?faction=${params.faction}`, {
-		method: 'POST',
-		headers: {
-			cookie: dcauth,
-			'Content-Type': 'application/json'
+	const post = await fetch(
+		`${apiUrl}/sys/config/faction-post?faction=${params.faction}`,
+		{
+			method: "POST",
+			headers: {
+				cookie: dcauth,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
 		},
-		body: JSON.stringify(body)
-	});
+	);
 	const text = await post.text();
 	return new Response(text, { status: post.status });
 };
--- web/src/routes/web-api/sys/global/change/+server.ts
+++ web/src/routes/web-api/sys/global/change/+server.ts
@@ -1,19 +1,19 @@
-import { apiUrl } from '$lib/api';
-import type { RequestHandler } from '@sveltejs/kit';
+import { apiUrl } from "$lib/api";
+import type { RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ request, cookies }) => {
-	const dcauth = cookies.get('auth_token') as string;
+	const dcauth = cookies.get("auth_token") as string;
 	const body: {
 		announcement: string | undefined;
 		maintenance: string | undefined;
 	} = await request.json();
-	const post = await fetch(`${apiUrl}/ucp/sys/config/global-post`, {
-		method: 'POST',
+	const post = await fetch(`${apiUrl}/sys/config/global-post`, {
+		method: "POST",
 		headers: {
 			cookie: dcauth,
-			'Content-Type': 'application/json'
+			"Content-Type": "application/json",
 		},
-		body: JSON.stringify(body)
+		body: JSON.stringify(body),
 	});
 	const text = await post.text();
 	return new Response(text, { status: post.status });
